fff1cc52837a4d5607a600e0fc4ec48b53a504cd
Morronel <zahar.kogut@gmail.com>
2018-11-10 01:34:02 +0200

    created source file and executable, which I need to modify

---
+++ while/while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+  int i;
+
+  i = 7;
+  while(i > 0){
+    i--;
+    printf("fun!\n");
+  }
+  if (i > 0){
+    printf("ASSEMBLER IS FUN!\n");
+  }
+  return 0;
+}
